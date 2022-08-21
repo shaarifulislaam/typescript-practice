ca742fe370da6b6981ef4a2f77784d1faae4d345
Shariful <sharifulmamun7@gmail.com>
2022-08-22 02:50:19 +0600

    initial commit

--- output/script.js
+++ output/script.js
@@ -86,7 +86,7 @@ obj = {
     age: 'fourty'
 };
 console.log(obj);
-// function type 
+//* function type 
 let my;
 my = () => {
     console.log('from my');
@@ -127,3 +127,78 @@ const myFunc6 = (a, b) => {
     return a + b;
 };
 myFunc6(5, 6);
+//* type aliases
+const userDetails = (id, user) => {
+    console.log(`User id ${id}, name is ${user.name} and age is ${user.age}`);
+};
+const sayHello = (user) => {
+    console.log(`Hello ${user.age > 50 ? "Sir" : "Mr"} ${user.name}`);
+};
+userDetails(10, { name: 'ss', age: 40 });
+sayHello({ name: 'dsfds', age: 40 });
+const userDetails2 = (id, user) => {
+    console.log(`User id ${id}, name is ${user.name} and age is ${user.age}`);
+};
+const sayHello2 = (user) => {
+    console.log(`Hello ${user.age > 50 ? "Sir" : "Mr"} ${user.name}`);
+};
+userDetails2(33, { name: 'sssss', age: 40 });
+sayHello2({ name: 'nnnn', age: 40 });
+//*function signature
+//*function er akta structure orthang amr function ta kmn hobe;
+let funtionMy; //its function signature..orthath aita parameter nibe and void return korbe...aita arrow function ar moto bt arrow function na
+let add; //aita 2 ta number add er parameter and number return krba tar signature
+add = (x, y) => {
+    return x + y;
+};
+console.log("Result is=", add(55, 45));
+let calculation;
+calculation = (x, y, z) => {
+    if (z === 'add') {
+        return x + y;
+    }
+    else {
+        return x - y;
+    }
+};
+console.log(calculation(30, 20, "add"));
+let calc;
+calc = (x, y, z) => {
+    return (z === "add") ? (x + y) : (x - y);
+};
+console.log(calc(50, 20, "minus"));
+let userInfo;
+userInfo = (id, userDetails) => {
+    return `user id ${id} , ${userDetails.age < 20 ? "Jr" : "Sr"} ${userDetails.name} and age is ${userDetails.age}`;
+};
+console.log(userInfo(202, { name: "Arfan", age: 22 }));
+//*class
+class Player {
+    constructor(n, a, c) {
+        this.name = n;
+        this.age = a;
+        this.country = c;
+    }
+    play() {
+        console.log(`${this.name} from ${this.country} is playing!`);
+    }
+}
+const tamim = new Player('Tamim', 36, 'BD');
+const afif = new Player('afif', 30, 'BD');
+const bijoy = new Player('bijoy', 20, 'BD');
+// console.log(tamim.name)
+const players = [];
+players.push(afif);
+players.push(bijoy);
+players.push(tamim);
+console.log(players);
+/* console.log(tamim);
+console.log(afif);
+console.log(bijoy); */
+//*access Modifier
+//*class ar kono property k jokon bahire theke access kora jay oita wired type..aita duer korar jnno access modifier...access modifer use krle oita sudu class ar maje access krta parbo ar bahire nah access modify krta jatake krbo oitar sathe private keyword jukto krta hobe..tahole oita k r class ar bahire access krte parbe nah
+// console.log(afif.name);
+// console.log(afif.age);
+console.log(afif.country);
+//private dile oita k class ar bahire access ow krta parbo na use ow krta parbo na
+// readonly khetre class ar bahire access krta parbo kintu change krta parbo nah
--- src/script.ts
+++ src/script.ts
@@ -125,7 +125,7 @@ obj ={
 }
 console.log(obj);
 
-// function type 
+//* function type 
 let my : Function;
 my = () =>{
     console.log('from my')
@@ -178,3 +178,125 @@ const myFunc6 = (a:number,b:number) : number=>{
 }
 
 myFunc6(5,6);
+
+
+//* type aliases
+
+const userDetails = (id:(number|string),user:{name: string,age:number}) =>{
+    console.log(`User id ${id}, name is ${user.name} and age is ${user.age}`);
+}
+
+const sayHello =(user:{name:string,age:number})=>{
+    console.log(`Hello ${user.age>50 ? "Sir" : "Mr"} ${user.name}`);
+}
+
+userDetails(10,{name:'ss',age:40});
+sayHello({name:'dsfds',age:40});
+
+//implement type aliases
+//type gulo aibabe declare kora thakle amra poyojone jekono jaygay use krta pari bar bar amader likha lagteche nah...akkaj bar bar kora hocce nah
+
+type stringOrNum = (number|string);
+type userType = {name: string,age:number}
+
+const userDetails2 = (id: stringOrNum,user:userType) =>{
+    console.log(`User id ${id}, name is ${user.name} and age is ${user.age}`);
+}
+
+const sayHello2 =(user:userType)=>{
+    console.log(`Hello ${user.age>50 ? "Sir" : "Mr"} ${user.name}`);
+}
+
+userDetails2(33,{name:'sssss',age:40});
+sayHello2({name:'nnnn',age:40});
+
+//*function signature
+//*function er akta structure orthang amr function ta kmn hobe;
+
+let funtionMy : ()=>void; //its function signature..orthath aita parameter nibe and void return korbe...aita arrow function ar moto bt arrow function na
+
+let add: (a:number,b:number) => number; //aita 2 ta number add er parameter and number return krba tar signature
+
+add = (x:number,y:number) =>{
+    return x+y;
+}
+console.log("Result is=",add(55,45));
+
+let calculation : (a:number, b:number,c:string) => number;
+
+calculation = (x:number,y:number,z:string) =>{
+    if(z==='add'){
+        return x+y;
+    }else{
+        return x-y;
+    }
+}
+console.log(calculation(30,20,"add"));
+
+let calc : (a:number,b:number,c:string) => number;
+
+calc = (x:number,y:number,z:string) =>{
+    return (z==="add") ? ( x+y) : (x-y);
+}
+
+console.log(calc(50,20,"minus"));
+
+let userInfo :(id:number|string , userDetails:{
+    name : string,
+    age:number
+}) => string;
+
+userInfo = (id:number | string , userDetails:{name: string , age:number}) =>{
+        return `user id ${id} , ${userDetails.age <20 ? "Jr" : "Sr"} ${userDetails.name} and age is ${userDetails.age}`
+}
+
+console.log(userInfo(202,{name:"Arfan",age:22}));
+
+
+//*class
+
+class Player {
+   private name: string;
+   private age: number;
+   readonly country : string;
+
+    constructor(n:string , a:number , c:string) {
+        this.name = n;
+        this.age = a;
+        this.country = c;
+    }
+    play (){
+        console.log(`${this.name} from ${this.country} is playing!`);
+    }
+}
+
+const tamim = new Player('Tamim',36,'BD');
+const afif = new Player('afif',30,'BD');
+const bijoy = new Player('bijoy',20,'BD');
+// console.log(tamim.name)
+
+const players : Player[] = [];
+
+players.push(afif);
+players.push(bijoy);
+players.push(tamim);
+
+
+console.log(players);
+
+/* console.log(tamim);
+console.log(afif);
+console.log(bijoy); */
+
+
+//*access Modifier
+
+//*class ar kono property k jokon bahire theke access kora jay oita wired type..aita duer korar jnno access modifier...access modifer use krle oita sudu class ar maje access krta parbo ar bahire nah access modify krta jatake krbo oitar sathe private keyword jukto krta hobe..tahole oita k r class ar bahire access krte parbe nah
+
+// console.log(afif.name);
+// console.log(afif.age);
+console.log(afif.country);
+
+//private dile oita k class ar bahire access ow krta parbo na use ow krta parbo na
+// readonly khetre class ar bahire access krta parbo kintu change krta parbo nah
+
